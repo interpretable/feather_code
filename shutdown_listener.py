2cf85d29a5d27557ac93f040043b1a8235ab86ab
Anthony Angelot <anthony.angelot@gmail.com>
2019-06-05 14:02:36 +0200

    Add shutdown

---
+++ shutdown_listener.py
@@ -0,0 +1,15 @@
+import socket
+import os
+
+UDP_IP = "192.168.1.2"
+UDP_PORT = 8888
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+print('bind ok')
+while True:
+	response, addr = sock.recvfrom(255)
+	response_str = response.decode("cp1252")
+	if len(response_str) == 9 :
+		os.system('shutdown')
+
+
